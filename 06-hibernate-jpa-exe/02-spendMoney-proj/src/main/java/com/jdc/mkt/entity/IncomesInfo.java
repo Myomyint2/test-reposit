b47db5bdeebabf4a877d9d51200ae54ffa682743
User <User@ASUS-Ryzen-5>
2025-12-12 15:36:13 +0630

    Testing

---
+++ 06-hibernate-jpa-exe/02-spendMoney-proj/src/main/java/com/jdc/mkt/entity/IncomesInfo.java
@@ -0,0 +1,34 @@
+package com.jdc.mkt.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "incomes_info_tbl")
+public class IncomesInfo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "incomes_type")
+	private IncomesType incomesType;
+	
+	private LocalDate date;
+	private String description;
+	
+	public enum IncomesType{
+		Salary,Bonus,Other
+	}
+}
